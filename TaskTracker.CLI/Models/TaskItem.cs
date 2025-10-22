68129ca3e2f58abaf6b201ed5062114bcb47b997
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-10-21 22:04:20 -0400

    Override `ToString` in TaskItem to display task details

--- TaskTracker.CLI/Models/TaskItem.cs
+++ TaskTracker.CLI/Models/TaskItem.cs
@@ -7,4 +7,6 @@ public class TaskItem
     public TaskStatus Status { get; set; } = TaskStatus.ToDo;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    
+    public override string ToString() => $"[{Id}] {Description} - {Status}";
 }
